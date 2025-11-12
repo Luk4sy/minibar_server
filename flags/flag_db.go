143bdc92d5e796dde6069d7a2bcec01805501604
Luk4sy <302597565@qq.com>
2025-11-12 15:35:11 +0800

    feat-文章表创建和迁移

--- flags/flag_db.go
+++ flags/flag_db.go
@@ -10,6 +10,7 @@ func FlagDB() {
 	err := global.DB.AutoMigrate(
 		&models.UserModel{},
 		&models.UserConfModel{},
+		&models.ArticleModel{},
 	)
 	if err != nil {
 		logrus.Errorf("数据库迁移失败 %s", err)
--- models/article_model.go
+++ models/article_model.go
@@ -1,1 +1,19 @@
 package models
+
+type ArticleModel struct {
+	Model
+	Title        string    `gorm:"size:32" json:"title"`
+	Abstract     string    `gorm:"size:256" json:"abstract"`
+	Content      string    `json:"content"`
+	CategoryID   uint      `json:"categoryID"`                                   // 分类的id
+	TagList      []string  `gorm:"type:longtext;serializer:json" json:"tagList"` // 标签列表
+	Cover        string    `gorm:"size:256" json:"cover"`
+	UserID       uint      `json:"userID"`
+	UserModel    UserModel `gorm:"foreignKey:UserID" json:"-"` // 关联的用户信息
+	LookCount    int       `json:"lookCount"`                  // 阅读数
+	DiggCount    int       `json:"diggCount"`                  // 点赞数
+	CommentCount int       `json:"commentCount"`               // 评论数
+	CollectCount int       `json:"collectCount"`               // 收藏数
+	OpenComment  bool      `json:"openComment"`                // 开启评论
+	Status       int8      `json:"status"`                     // 状态 草稿 审核中 已发布
+}
