05cf66b79753319906a108155cb6d6e2ed4b4be7
Luk4sy <302597565@qq.com>
2025-11-12 19:43:35 +0800

    feat-登录失败日志管理

--- service/log_service/login_log.go
+++ service/log_service/login_log.go
@@ -31,3 +31,20 @@ func NewLoginSuccess(c *gin.Context, loginType enum.LoginType) {
 		LoginType:   loginType,
 	})
 }
+
+func NewLoginFail(c *gin.Context, loginType enum.LoginType, msg string, username string, pwd string) {
+	ip := c.ClientIP()
+	addr := core.GetIpAddr(ip)
+
+	global.DB.Create(&models.LogModel{
+		LogType:     enum.LoginLogType,
+		Title:       "用户登录失败",
+		Content:     msg,
+		IP:          ip,
+		Addr:        addr,
+		LoginStatus: false,
+		Username:    username,
+		Pwd:         pwd,
+		LoginType:   loginType,
+	})
+}
