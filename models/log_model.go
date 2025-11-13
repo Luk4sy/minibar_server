216016eaffda43d729873aa35f6ea85358408b5b
Luk4sy <302597565@qq.com>
2025-11-13 18:34:20 +0800

    feat-操作日志基本框架

--- models/log_model.go
+++ models/log_model.go
@@ -4,17 +4,17 @@ import "blogx_server/models/enum"
 
 type LogModel struct {
 	Model
-	LogType     enum.LogType   `json:"logType"`                    // 日志类型
-	Title       string         `gorm:"size:64" json:"title"`       // 日志标题
-	Content     string         `json:"content"`                    // 日志内容
-	Level       int8           `json:"level"`                      // 日志级别
-	UserID      uint           `json:"userID"`                     // 用户ID
-	UserModel   UserModel      `gorm:"foreignKey:UserID" json:"-"` // 关联用户信息
-	IP          string         `gorm:"size:32" json:"ip"`          // 操作IP
-	Addr        string         `gorm:"size:64" json:"addr"`        // IP归属地
-	IsRead      bool           `json:"isRead"`                     // 是否读取
-	LoginStatus bool           `json:"loginStatus"`                // 登陆状态
-	Username    string         `json:"username"`                   // 登录日志的用户名
-	Pwd         string         `json:"pwd"`                        // 登录日志的密码
-	LoginType   enum.LoginType `json:"loginType"`                  // 登陆的类型
+	LogType     enum.LogType      `json:"logType"`                    // 日志类型
+	Title       string            `gorm:"size:64" json:"title"`       // 日志标题
+	Content     string            `json:"content"`                    // 日志内容
+	Level       enum.LogLevelType `json:"level"`                      // 日志级别
+	UserID      uint              `json:"userID"`                     // 用户ID
+	UserModel   UserModel         `gorm:"foreignKey:UserID" json:"-"` // 关联用户信息
+	IP          string            `gorm:"size:32" json:"ip"`          // 操作IP
+	Addr        string            `gorm:"size:64" json:"addr"`        // IP归属地
+	IsRead      bool              `json:"isRead"`                     // 是否读取
+	LoginStatus bool              `json:"loginStatus"`                // 登陆状态
+	Username    string            `json:"username"`                   // 登录日志的用户名
+	Pwd         string            `json:"pwd"`                        // 登录日志的密码
+	LoginType   enum.LoginType    `json:"loginType"`                  // 登陆的类型
 }
---
+++ service/log_service/action_log.go
@@ -0,0 +1,49 @@
+package log_service
+
+import (
+	"blogx_server/core"
+	"blogx_server/global"
+	"blogx_server/models"
+	"blogx_server/models/enum"
+	"github.com/gin-gonic/gin"
+	"github.com/sirupsen/logrus"
+)
+
+type ActionLog struct {
+	c     *gin.Context
+	level enum.LogLevelType
+	title string
+}
+
+func (ac *ActionLog) SetTitle(title string) {
+	ac.title = title
+}
+func (ac *ActionLog) SeteLevel(level enum.LogLevelType) {
+	ac.level = level
+}
+
+func (ac *ActionLog) Save() {
+	ip := ac.c.ClientIP()
+	addr := core.GetIpAddr(ip)
+	userID := uint(1)
+
+	err := global.DB.Create(&models.LogModel{
+		LogType: enum.ActionLogType,
+		Title:   ac.title,
+		Content: "",
+		Level:   ac.level,
+		UserID:  userID,
+		IP:      ip,
+		Addr:    addr,
+	})
+	if err != nil {
+		logrus.Errorf("日志创建失败 %s", err)
+		return
+	}
+}
+
+func NewActionLogByGin(c *gin.Context) *ActionLog {
+	return &ActionLog{
+		c: c,
+	}
+}
