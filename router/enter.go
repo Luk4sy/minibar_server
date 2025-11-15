56f54ef828cef7ab43c2f84776bbf4f7e8372802
Luk4sy <302597565@qq.com>
2025-11-15 11:07:41 +0800

    feat-日志管理的分页查询功能

--- router/enter.go
+++ router/enter.go
@@ -7,12 +7,16 @@ import (
 )
 
 func Run() {
-	r := gin.Default() // 1️⃣ 创建一个默认的 Gin 路由引擎（带 Logger 和 Recovery 中间件）
+	gin.SetMode(global.Config.System.GinMode) // 配置路由模式
+	r := gin.Default()                        // 1️⃣ 创建一个默认的 Gin 路由引擎（带 Logger 和 Recovery 中间件）
+
+	r.Static("/uploads", "uploads") // 静态路由
 
 	nr := r.Group("/api") // 2️⃣ 创建一个路由分组，所有接口都将以 "/api" 开头
 
 	nr.Use(middleware.LogMiddleware)
-	SiteRouter(nr) // 3️⃣ 注册具体的路由模块，比如网站信息接口
+	SiteRouter(nr) // 3️⃣ 注册具体的路由模块，站点模块
+	LogRouter(nr)  // 3️⃣ 注册具体的路由模块，日志模块
 
 	addr := global.Config.System.Addr() // 4️⃣ 从全局配置读取服务启动地址（如 :8080）
 	r.Run(addr)                         // 5️⃣ 启动 HTTP 服务
