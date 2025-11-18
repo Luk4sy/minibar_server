6a622fa5c23b4cd9e5d99139bec2714a6de78053
Luk4sy <302597565@qq.com>
2025-11-18 21:21:57 +0800

    feat-站点配置信息接口

--- api/site_api/enter.go
+++ api/site_api/enter.go
@@ -2,14 +2,56 @@ package site_api
 
 import (
 	"blogx_server/common/res"
+	"blogx_server/global"
+	"blogx_server/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 type SiteApi struct {
 }
 
+type SiteInfoRequest struct {
+	Name string `uri:"name"`
+}
+
 func (SiteApi) SiteInfoView(c *gin.Context) {
-	res.OkWithData("xx", c)
+	var cr SiteInfoRequest
+	err := c.ShouldBindUri(&cr)
+	if err != nil {
+		res.FailWithError(err, c)
+		return
+	}
+
+	if cr.Name == "site" {
+		res.OkWithData(global.Config.Site, c)
+		return
+	}
+
+	// 判断角色是不是管理员
+	middleware.AdminMiddleware(c)
+
+	_, ok := c.Get("claims")
+	if !ok {
+		return
+	}
+
+	var data any
+	switch cr.Name {
+	case "site":
+		data = global.Config.Site
+	case "email":
+		data = global.Config.Email
+	case "qq":
+		data = global.Config.QQ
+	case "qiNiu":
+		data = global.Config.QiNiu
+	case "ai":
+		data = global.Config.Ai
+	default:
+		res.FailWithMsg("不存在的配置", c)
+		return
+	}
+	res.OkWithData(data, c)
 	return
 }
 
