059b04cd1d54aaaf5784fe8c3569d47bc8adc8cd
Luk4sy <302597565@qq.com>
2025-11-18 20:33:38 +0800

    feat-站点信息配置

---
+++ conf/conf_site.go
@@ -0,0 +1,58 @@
+package conf
+
+type Site struct {
+	SiteInfo   SiteInfo   `yaml:"siteInfo" json:"siteInfo"`
+	Project    Project    `yaml:"project" json:"project"`
+	Seo        Seo        `yaml:"seo" json:"seo"`
+	About      About      `yaml:"about" json:"about"`
+	Login      Login      `yaml:"login" json:"login"`
+	IndexRight IndexRight `yaml:"indexRight" json:"indexRight"`
+	Article    Article    `yaml:"article" json:"article"`
+}
+
+type SiteInfo struct {
+	Title string `yaml:"title" json:"title"`
+	Logo  string `yaml:"logo" json:"logo"`
+	IcpNo string `yaml:"icpNo" json:"icpNo"`
+	Mode  int8   `yaml:"mode" json:"mode"` // 1 社区模式 2 博客模式
+}
+
+type Project struct {
+	Title   string `yaml:"title" json:"title"`
+	Icon    string `yaml:"icon" json:"icon"`
+	WebPath string `yaml:"webPath" json:"webPath"`
+}
+
+type Seo struct {
+	Keywords    string `yaml:"keywords" json:"keywords"`
+	Description string `yaml:"description" json:"description"`
+}
+
+type About struct {
+	SiteDate       string `yaml:"siteDate" json:"siteDate"` // 年月日
+	QQQrCode       string `yaml:"qqQrCode" json:"QQQrCode"`
+	WechatQrCode   string `yaml:"wechatQrCode" json:"wechatQrCode"`
+	GiteeQrCode    string `yaml:"giteeQrCode" json:"giteeQrCode"`
+	BilibiliQrCode string `yaml:"bilibiliQrCode" json:"bilibiliQrCode"`
+	GithubQr       string `yaml:"githubQr" json:"githubQr"`
+}
+
+type Login struct {
+	QQLogin          bool `yaml:"qqLogin" json:"qqLogin"`
+	UsernamePwdLogin bool `yaml:"usernamePwdLogin" json:"usernamePwdLogin"`
+	email            bool `yaml:"email" json:"email"`
+	Captcha          bool `yaml:"captcha" json:"captcha"`
+}
+
+type ComponentInfo struct {
+	Title  string `yaml:"title" json:"title"`
+	Enable bool   `yaml:"enable" json:"enable"`
+}
+
+type IndexRight struct {
+	List []ComponentInfo `json:"list" yaml:"list"`
+}
+
+type Article struct {
+	IsFreeReview bool `json:"isFreeReview" yaml:"isFreeReview"` // 是否免审核
+}
