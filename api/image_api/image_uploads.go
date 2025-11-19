e5eef7e4cd40bd8676bee2e525d572a2553fe476
Luk4sy <302597565@qq.com>
2025-11-19 19:25:39 +0800

    feat-文件大小判断和文件非法判断

--- api/image_api/image_uploads.go
+++ api/image_api/image_uploads.go
@@ -2,8 +2,12 @@ package image_api
 
 import (
 	"blogx_server/common/res"
+	"blogx_server/global"
+	"blogx_server/utils"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"strings"
 )
 
 func (ImageApi) ImageUploadView(c *gin.Context) {
@@ -12,8 +16,33 @@ func (ImageApi) ImageUploadView(c *gin.Context) {
 		res.FailWithError(err, c)
 		return
 	}
+	// 文件大小判断
+	s := global.Config.Upload.Size
+	if fileHeader.Size > s*1024*1024 {
+		res.FailWithMsg(fmt.Sprintf("文件大小大于 %dMB", s), c)
+		return
+	}
+	// 后缀判断
+	filename := fileHeader.Filename
+	err = ImageSuffixJudge(filename)
+	if err != nil {
+		res.FailWithError(err, c)
+		return
+	}
 
 	filePath := fmt.Sprintf("uploads/images/%s", fileHeader.Filename)
 	c.SaveUploadedFile(fileHeader, filePath)
 	res.Ok("/"+filePath, "图片上传成功", c)
+}
+
+func ImageSuffixJudge(filename string) (err error) {
+	_list := strings.Split(filename, ".")
+	if len(_list) == 1 {
+		return errors.New("错误的文件名")
+	}
+	suffix := _list[len(_list)-1]
+	if !utils.InList(suffix, global.Config.Upload.WhiteList) {
+		return errors.New("文件非法")
+	}
+	return
 }
